7b97ce21017693e8508455c9dd9b909f4921d4df
Wasim Raza <wasimraza1698@gmail.com>
2020-12-16 09:42:28 +0530

    third

---
+++ PharmacyMedicineSupplyService/Repository/SupplyRepo.cs
@@ -0,0 +1,26 @@
+﻿using PharmacyMedicineSupplyService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PharmacyMedicineSupplyService.Repository
+{
+    public class SupplyRepo : ISupplyRepo
+    {
+        private readonly List<string> pharmacyNames = new List<string>() {"Appolo Pharmacy","Gupta Pharmacies","G.K Pharmacies" };
+        private readonly List<PharmacyDTO> pharmacies = new List<PharmacyDTO>();
+        public List<PharmacyDTO> GetPharmacies()
+        {
+           foreach(var i in pharmacyNames)
+           {
+                pharmacies.Add(new PharmacyDTO { pharmacyName = i });
+           }
+           return pharmacies;
+        }
+    }
+    
+}
